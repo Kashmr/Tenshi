0b60d76d10ed77fb26ac11374ecefc40bf7b7c70
Kashmr <f00x00c1@gmail.com>
2015-05-30 19:50:14 -0300

    list app dates basic functionality
    Adding to list_app_dates.php script. MySQL error on line 14. Needs a
    fix.

--- list_app_dates.php
+++ list_app_dates.php
@@ -1,3 +1,25 @@
 <?php
+	include("dbf/connection.php");
+
+	//set timezone to GTM-3 (Arg - Buenos Aires)
+	date_default_timezone_set("America/Argentina/Buenos_Aires");
+
+	//creates a MySQL connection
+	$con = connection($db_info);
+
+	$sql = "SELET * FROM appointments WHERE date = ".date('Y/m/d')."";
+
+	$result = mysqli_query($con,$sql);
+
+	if(mysqli_num_rows($result) > 0)
+	{
+		while($row = mysqli_fetch_assoc($result) )
+		{
+			var_dump($row);
+			echo "<br />";
+		}
+
+	}
+
 	
-	
+	//echo date("Y/m/d");
